2ffaec14cf1b09e01c9f8c995f7cdd223a93b131
Jason Hamilton <jasonvinh98@gmail.com>
2019-10-05 21:41:15 -0400

    Update Sleigh.h
    Add skeleton for sleigh header.

---
+++ Sleigh.h
@@ -0,0 +1,12 @@
+#ifndef DRIVINGSIMULATOR_SLEIGH_H
+#define DRIVINGSIMULATOR_SLEIGH_H
+
+#include "Vehicle.h"
+
+class Sleigh : public Vehicle
+{
+private:
+public:
+};
+
+#endif //DRIVINGSIMULATOR_SLEIGH_H
