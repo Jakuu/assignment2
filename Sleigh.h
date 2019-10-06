60368c3d7488f5f8e61165527ac80c787bb58399
Jason Hamilton <jasonvinh98@gmail.com>
2019-10-05 21:43:47 -0400

    Add header functions
    Add functions to Sleigh header file.

--- Sleigh.h
+++ Sleigh.h
@@ -6,7 +6,17 @@
 class Sleigh : public Vehicle
 {
 private:
+	int myReindeerCount;
 public:
+	explicit Sleigh(string brand, string model, int reindeerCount = 1);
+
+	virtual ~Sleigh();
+	int getReindeerCount();
+	void setReindeerCount(int deerCount);
+	virtual double mileageEstimate(double time);
+
+	virtual string toString();
+
 };
 
 #endif //DRIVINGSIMULATOR_SLEIGH_H
